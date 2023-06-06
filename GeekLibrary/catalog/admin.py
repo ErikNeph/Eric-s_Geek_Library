a60dbf35129da1dd63d7e7f22510c3d051edece5
ErikNeph <erikgafurovtvorec@gmail.com>
2023-06-06 13:34:31 +0600

    Сделал сортировку более понятной в админке.

--- GeekLibrary/catalog/admin.py
+++ GeekLibrary/catalog/admin.py
@@ -11,17 +11,32 @@ admin.site.register(Status)
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name', 'first_name')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'genre', 'language', 'display_author')
+    list_filter = ('genre', 'author')
+    inlines = [BookInstanceInline]
 
 
 @admin.register(BookInstance)
 class BookInstance(admin.ModelAdmin):
-    pass
+    list_filter = ('book', 'status')
+    fieldsets = (
+        ('Экземпляр книги', {
+            'fields': ('book', 'imprint', 'inv_nom')
+        }),
+        ('Статус и окончание его действия', {
+            'fields': ('status', 'due_back')
+        })
+    )
 
 
 admin.site.register(Author, AuthorAdmin)

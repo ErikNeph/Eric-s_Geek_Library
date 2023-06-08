30fe68c22ffb4a993434830bff1374260ce0444e
ErikNeph <erikgafurovtvorec@gmail.com>
2023-06-08 17:03:28 +0600

    Написал шаблон для логина на сайт, так же в settings.py добавил путь к папке registration, затем сделал переадресацию на главную страницу сайта.

--- GeekLibrary/GeekLibrary/settings.py
+++ GeekLibrary/GeekLibrary/settings.py
@@ -34,7 +34,7 @@ ROOT_URLCONF = 'GeekLibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,3 +85,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+# Переадресация на главную страницу сайта после входа в систему
+LOGIN_REDIRECT_URL = '/'

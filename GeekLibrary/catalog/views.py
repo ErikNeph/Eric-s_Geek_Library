711d9f942a7932c10031c46e6c645bffa07f27bd
ErikNeph <erikgafurovtvorec@gmail.com>
2023-06-24 15:35:06 +0600

    Убрал лишний импорт.

--- GeekLibrary/catalog/views.py
+++ GeekLibrary/catalog/views.py
@@ -4,7 +4,6 @@ from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import ObjectDoesNotExist
 from .models import Book, Author, BookInstance, Genre
 from .forms import AuthorsForm
 
